2a4c03e36b00870105ad79f8fdeb2b34ad8f98c7
Dimes2212 <raduloffd@gmail.com>
2025-03-24 19:14:44 +0300

    начал делать скрипты

--- Assets/скрипты/VRCharacterController.cs
+++ Assets/скрипты/VRCharacterController.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+  
+    public Transform headTarget; // XR Origin Camera
+    public Transform leftHandTarget; // Левый контроллер
+    public Transform rightHandTarget; // Правый контроллер
 
-    // Update is called once per frame
+    public Transform characterHead; // Голова модели
+    public Transform characterLeftHand; // Левая рука модели
+    public Transform characterRightHand; // Правая рука модели
+
     void Update()
     {
-        
+        if (headTarget && characterHead)
+            characterHead.position = headTarget.position;
+
+        if (leftHandTarget && characterLeftHand)
+            characterLeftHand.position = leftHandTarget.position;
+
+        if (rightHandTarget && characterRightHand)
+            characterRightHand.position = rightHandTarget.position;
     }
+
+
 }
