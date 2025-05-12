09404c85c8c2dd05f44b0302fda323d9737f981f
Dimes2212 <raduloffd@gmail.com>
2025-05-12 21:02:48 +0300

    чуть поломался рабочий скелет

--- Assets/скрипты/EnemyStates/AttackState.cs
+++ Assets/скрипты/EnemyStates/AttackState.cs
@@ -5,22 +5,15 @@ public class AttackState : BaseState
     public override void EnterState(EnemyStateManager manager)
     {
         manager.SetSpeed(0);
-        manager.animator.SetBool("IsIdle", false);
-        manager.animator.SetBool("IsPatrolling", false);
-        manager.animator.SetBool("IsAgro", false);
         manager.animator.SetBool("IsAttack", true);
     }
-
-    public override void ExitState(EnemyStateManager manager)
-    {
-    }
-
+    public override void ExitState(EnemyStateManager manager) { }
     public override void UpdateState(EnemyStateManager manager)
     {
-        float distance = manager.DistanceToTarget();
-
-        
-
-        
+        if (manager.DistanceToTarget() >= manager.attackDistance)
+        {
+            manager.SwitchState(manager.agroState);
+            return;
+        }
     }
 }
