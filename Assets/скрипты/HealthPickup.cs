b05c9a1685e4d25df4052afaf0161f3ca77b77c2
Dimes2212 <raduloffd@gmail.com>
2025-05-20 12:39:22 +0300

    пульки

---
+++ Assets/скрипты/HealthPickup.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class HealthPickup : MonoBehaviour
+{
+    [Header("Настройки аптечки")]
+    public float healAmount = 25f;
+    public AudioClip healSound;
+    public ParticleSystem healEffect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        Health health = other.GetComponent<Health>();
+        if (health != null)
+        {
+            health.Heal(healAmount);
+
+            if (healSound != null)
+                AudioSource.PlayClipAtPoint(healSound, transform.position);
+
+            if (healEffect != null)
+                Instantiate(healEffect, transform.position, Quaternion.identity);
+
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/скрипты/PlayerActions/Health.cs
+++ Assets/скрипты/PlayerActions/Health.cs
@@ -1,7 +1,86 @@
+////using UnityEngine;
+////using UnityEngine.Events;
+////using TMPro;
+////using UnityEngine.SceneManagement; 
+
+////public class Health : MonoBehaviour
+////{
+////    public float maxHealth = 100f;
+////    private float currentHealth;
+////    public UnityEvent onDamage;
+////    public UnityEvent onDeath;
+////    public TextMeshProUGUI hologramText;
+////    public int rewardOnDeath = 0;
+////    [SerializeField] private string deathSceneName = "GameOver"; 
+////    private Animator animator;
+////    private SimpleEnemyStateManager simpleEnemyStateManager;
+////    private EnemyStateManager enemyStateManager;
+
+////    void Awake()
+////    {
+////        currentHealth = maxHealth;
+////        animator = GetComponent<Animator>();
+////        simpleEnemyStateManager = GetComponent<SimpleEnemyStateManager>();
+////        enemyStateManager = GetComponent<EnemyStateManager>();
+////        UpdateHologram();
+////    }
+
+////    public void TakeDamage(float amount)
+////    {
+////        if (currentHealth <= 0) return;
+
+////        currentHealth = Mathf.Max(currentHealth - amount, 0f);
+////        onDamage?.Invoke();
+////        UpdateHologram();
+
+////        if (currentHealth <= 0)
+////            Die();
+////    }
+
+////    void Die()
+////    {
+////        onDeath?.Invoke();
+
+////        if (animator != null)
+////        {
+////            animator.Play("Death", -1, 0f);
+////            animator.ResetTrigger("Die");
+////            animator.SetTrigger("Die");
+////        }
+
+////        if (simpleEnemyStateManager != null)
+////        {
+////            simpleEnemyStateManager.Die();
+////        }
+////        else if (enemyStateManager != null)
+////        {
+////            enemyStateManager.Die();
+////        }
+
+
+////        if ((currentHealth <= 0) && (simpleEnemyStateManager == null) && (enemyStateManager == null))
+////        {
+
+////        }
+////    }
+
+
+
+////    void UpdateHologram()
+////    {
+////        if (hologramText == null) return;
+////        hologramText.text = $"HP: {Mathf.Ceil(currentHealth)}";
+////        hologramText.color = currentHealth <= maxHealth * 0.3f ? Color.red : Color.white;
+////    }
+
+////    public float GetHealth() => currentHealth;
+////    public float GetHealthNormalized() => currentHealth / maxHealth;
+////}
+
+
 //using UnityEngine;
 //using UnityEngine.Events;
 //using TMPro;
-//using UnityEngine.SceneManagement; 
 
 //public class Health : MonoBehaviour
 //{
@@ -11,10 +90,18 @@
 //    public UnityEvent onDeath;
 //    public TextMeshProUGUI hologramText;
 //    public int rewardOnDeath = 0;
-//    [SerializeField] private string deathSceneName = "GameOver"; 
+
+//    [SerializeField] private EnemyZoneCleaner enemyZoneCleaner;
+
+//    [SerializeField] private Transform deathZonePoint;     // Точка телепорта в зону смерти
+//    [SerializeField] private Transform respawnPoint;       // Точка возврата в игру
+//    [SerializeField] private GameObject deathMenuUI;       // Меню смерти (UI Canvas)
+
 //    private Animator animator;
 //    private SimpleEnemyStateManager simpleEnemyStateManager;
 //    private EnemyStateManager enemyStateManager;
+
+//    private bool isPlayer => animator == null && simpleEnemyStateManager == null && enemyStateManager == null;
 
 //    void Awake()
 //    {
@@ -41,6 +128,7 @@
 //    {
 //        onDeath?.Invoke();
 
+//        // Враг
 //        if (animator != null)
 //        {
 //            animator.Play("Death", -1, 0f);
@@ -51,30 +139,54 @@
 //        if (simpleEnemyStateManager != null)
 //        {
 //            simpleEnemyStateManager.Die();
+//            return;
 //        }
-//        else if (enemyStateManager != null)
+
+//        if (enemyStateManager != null)
 //        {
 //            enemyStateManager.Die();
+//            return;
 //        }
 
-
-//        if ((currentHealth <= 0) && (simpleEnemyStateManager == null) && (enemyStateManager == null))
+//        // Игрок
+//        if (isPlayer && deathZonePoint != null)
 //        {
+//            // Телепорт в зону смерти
+//            transform.position = deathZonePoint.position;
+//            transform.rotation = deathZonePoint.rotation;
 
+//            // Показываем меню смерти
+//            if (deathMenuUI != null)
+//                deathMenuUI.SetActive(true);
+//            // Удаляем врагов в зоне, если есть ссылка
+//            if (enemyZoneCleaner != null)
+//            {
+//                enemyZoneCleaner.ClearEnemiesInZone();
+//            }
+
+//            // Останавливаем игру
+//            Time.timeScale = 0f;
 //        }
 //    }
-
-
 
 //    void UpdateHologram()
 //    {
 //        if (hologramText == null) return;
+
 //        hologramText.text = $"HP: {Mathf.Ceil(currentHealth)}";
 //        hologramText.color = currentHealth <= maxHealth * 0.3f ? Color.red : Color.white;
 //    }
 
+//    public void RestoreHealth()
+//    {
+//        currentHealth = maxHealth;
+//        UpdateHologram();
+//    }
+
 //    public float GetHealth() => currentHealth;
 //    public float GetHealthNormalized() => currentHealth / maxHealth;
+
+//    public Transform GetRespawnPoint() => respawnPoint;
 //}
 
 
@@ -92,10 +204,9 @@ public class Health : MonoBehaviour
     public int rewardOnDeath = 0;
 
     [SerializeField] private EnemyZoneCleaner enemyZoneCleaner;
-
-    [SerializeField] private Transform deathZonePoint;     // Точка телепорта в зону смерти
-    [SerializeField] private Transform respawnPoint;       // Точка возврата в игру
-    [SerializeField] private GameObject deathMenuUI;       // Меню смерти (UI Canvas)
+    [SerializeField] private Transform deathZonePoint;
+    [SerializeField] private Transform respawnPoint;
+    [SerializeField] private GameObject deathMenuUI;
 
     private Animator animator;
     private SimpleEnemyStateManager simpleEnemyStateManager;
@@ -124,11 +235,24 @@ public class Health : MonoBehaviour
             Die();
     }
 
+    public void Heal(float amount)
+    {
+        if (currentHealth <= 0) return;
+
+        currentHealth = Mathf.Min(currentHealth + amount, maxHealth);
+        UpdateHologram();
+    }
+
+    public void RestoreHealth()
+    {
+        currentHealth = maxHealth;
+        UpdateHologram();
+    }
+
     void Die()
     {
         onDeath?.Invoke();
 
-        // Враг
         if (animator != null)
         {
             animator.Play("Death", -1, 0f);
@@ -148,23 +272,17 @@ public class Health : MonoBehaviour
             return;
         }
 
-        // Игрок
         if (isPlayer && deathZonePoint != null)
         {
-            // Телепорт в зону смерти
             transform.position = deathZonePoint.position;
             transform.rotation = deathZonePoint.rotation;
 
-            // Показываем меню смерти
             if (deathMenuUI != null)
                 deathMenuUI.SetActive(true);
-            // Удаляем врагов в зоне, если есть ссылка
-            if (enemyZoneCleaner != null)
-            {
-                enemyZoneCleaner.ClearEnemiesInZone();
-            }
 
-            // Останавливаем игру
+            if (enemyZoneCleaner != null)
+                enemyZoneCleaner.ClearEnemiesInZone();
+
             Time.timeScale = 0f;
         }
     }
@@ -177,14 +295,7 @@ public class Health : MonoBehaviour
         hologramText.color = currentHealth <= maxHealth * 0.3f ? Color.red : Color.white;
     }
 
-    public void RestoreHealth()
-    {
-        currentHealth = maxHealth;
-        UpdateHologram();
-    }
-
     public float GetHealth() => currentHealth;
     public float GetHealthNormalized() => currentHealth / maxHealth;
-
     public Transform GetRespawnPoint() => respawnPoint;
 }
