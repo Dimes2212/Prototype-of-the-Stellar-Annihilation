0e2337dd01d55f883421fa583dbe6888a9bea0d7
Dimes2212 <raduloffd@gmail.com>
2025-05-31 19:44:27 +0300

    пули и анимация смерти

--- Assets/скрипты/EnemyStates/door/DoorDeathState.cs
+++ Assets/скрипты/EnemyStates/door/DoorDeathState.cs
@@ -1,6 +1,35 @@
 
 
 
+//using UnityEngine;
+
+//public class DoorDeathState : DoorBaseState
+//{
+//    public override void OnEnter(SimpleEnemyStateManager manager)
+//    {
+//        manager.GetComponent<EnemyDeathNotifier>()?.NotifyDeath();
+//        Debug.Log("Entering Death State");
+//        // Отключаем движение
+//        //manager.navMeshAgent.isStopped = true;
+//        //manager.navMeshAgent.enabled = false;
+
+
+//        //manager.animator.ResetTrigger("Die");
+//        manager.animator.SetTrigger("Die");
+
+
+
+//    }
+
+//    public override void OnUpdate(SimpleEnemyStateManager manager)
+//    {
+
+//    }
+
+//    public override void OnExit(SimpleEnemyStateManager manager) { }
+//}
+
+
 using UnityEngine;
 
 public class DoorDeathState : DoorBaseState
@@ -9,21 +38,24 @@ public class DoorDeathState : DoorBaseState
     {
         manager.GetComponent<EnemyDeathNotifier>()?.NotifyDeath();
         Debug.Log("Entering Death State");
-        // Отключаем движение
-        //manager.navMeshAgent.isStopped = true;
-        //manager.navMeshAgent.enabled = false;
 
-      
-        //manager.animator.ResetTrigger("Die");
+        // Анимация смерти
         manager.animator.SetTrigger("Die");
-        
 
-       
+        // Выключаем все коллайдеры
+        Collider[] colliders = manager.GetComponentsInChildren<Collider>();
+        foreach (Collider col in colliders)
+        {
+            col.enabled = false;
+        }
+
+        // Уничтожаем объект через 5 секунд
+        Object.Destroy(manager.gameObject, 5f);
     }
 
     public override void OnUpdate(SimpleEnemyStateManager manager)
     {
-       
+        // Ничего не делаем в состоянии смерти
     }
 
     public override void OnExit(SimpleEnemyStateManager manager) { }
--- Assets/скрипты/Guns/BulletSelfDestruct.cs
+++ Assets/скрипты/Guns/BulletSelfDestruct.cs
@@ -1,28 +1,79 @@
 
 
+//using UnityEngine;
+
+//public class BulletSelfDestruct : MonoBehaviour
+//{
+//    public float speed = 20f;
+//    public float lifetime = 5f;
+
+//    void Start()
+//    {
+//        Destroy(gameObject, lifetime); // Удалить пулю через lifetime секунд
+//    }
+
+//    void Update()
+//    {
+//        transform.position += transform.forward * speed * Time.deltaTime;
+//    }
+
+//    void OnTriggerEnter(Collider other)
+//    {
+//        if (other.CompareTag("Weapon")) return;
+
+//        // Здесь можно добавить проверку, например:
+//        // if (other.CompareTag("Enemy")) { other.GetComponent<Health>().TakeDamage(damage); }
+
+//        Destroy(gameObject);
+//    }
+//}
+
+
 using UnityEngine;
 
 public class BulletSelfDestruct : MonoBehaviour
 {
     public float speed = 20f;
     public float lifetime = 5f;
+    public LayerMask collisionMask; // Укажи, с какими слоями пуля должна сталкиваться
 
-    void Start()
+    private void Start()
     {
-        Destroy(gameObject, lifetime); // Удалить пулю через lifetime секунд
+        Destroy(gameObject, lifetime);
     }
 
-    void Update()
+    private void Update()
     {
-        transform.position += transform.forward * speed * Time.deltaTime;
+        float moveDistance = speed * Time.deltaTime;
+        CheckCollision(moveDistance);
+        transform.Translate(Vector3.forward * moveDistance);
+    }
+
+    void CheckCollision(float moveDistance)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, moveDistance, collisionMask))
+        {
+            OnHitObject(hit);
+        }
+    }
+
+    void OnHitObject(RaycastHit hit)
+    {
+        // Можно добавить проверку типа объекта
+        if (hit.collider.CompareTag("Weapon")) return;
+
+        // Например:
+        // if (hit.collider.CompareTag("Enemy")) { hit.collider.GetComponent<Health>().TakeDamage(damage); }
+
+        Destroy(gameObject);
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Weapon")) return;
-
-        // Здесь можно добавить проверку, например:
-        // if (other.CompareTag("Enemy")) { other.GetComponent<Health>().TakeDamage(damage); }
 
         Destroy(gameObject);
     }
